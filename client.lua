9fb20d8e9f333e757dc5fa73960c6ac92a9a22e1
Gavi1223 <101108161+Gavi1223@users.noreply.github.com>
2022-07-24 07:56:56 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,79 +1,29 @@
+
 Citizen.CreateThread(function()
-	exports["drp-target"]:AddCircleZone("dodobox-buy", vector3(-442.5480, -2820.1885, 5.8244), 0.4, {
-			name = "dodobox-buy",
-			}, {
-			options = {
-				{
-					event = "dodobox-buymenu",
-					icon = "fas fa-circle",
-					label = "Buy Dodo Packages",
-				},
-			},
-			job = { "all" },
-			distance = 2.0
-		})
+    while true do
+        Citizen.Wait(0)
+        local ped = PlayerPedId()
+        local pedveh = GetVehiclePedIsIn(ped, false)
+        local pedvehclass = GetVehicleClass(pedveh)
+
+        if IsPedInAnyVehicle(ped) then
+            if ((pedvehclass ~= 8) and (pedvehclass ~= 13)) then
+                if IsPedDoingDriveby(ped) then
+                    local context = GetCamActiveViewModeContext()
+                    SetCamViewModeForContext(context, 4)
+                    changedViewMode = true
+                end
+            elseif ((pedvehclass == 8) and (pedvehclass == 13)) then
+            end
+        end
+
+    end
+
 end)
 
 
-
-
-
-RegisterNetEvent('dodobox-buymenu')
-AddEventHandler('dodobox-buymenu', function()
-
-	TriggerEvent('drp-context:sendMenu', {
-		{
-            id = 1,
-            header = "Small Package",
-			txt = "Buy a Small Dodo Package.",
-			params = {
-                event = "dodobox-getsmallpackage"
-            }
-        },
-        {
-            id = 2,
-            header = "Medium Package",
-			txt = "Buy a Medium Dodo Package.",
-			params = {
-                event = "dodobox-getmediumpackage"
-            }
-        },
-        {
-            id = 3,
-            header = "Large Package",
-			txt = "Buy a Large Dodo Package.",
-			params = {
-                event = "dodobox-getlargepackage"
-            }
-        },
-    })
-end)
-
-
-
-
-
-
-RegisterNetEvent('dodobox-getsmallpackage')
-AddEventHandler('dodobox-getsmallpackage', function(pItemId)
-  TriggerEvent("player:receiveItem", "dodopackagesmall", 1)
-  TriggerServerEvent('dodobox-deductsmallpackage')
-end)
-
-
-
-RegisterNetEvent('dodobox-getmediumpackage')
-AddEventHandler('dodobox-getmediumpackage', function(pItemId)
-    TriggerEvent("player:receiveItem", "dodopackagemedium", 1)
-    TriggerServerEvent('dodobox-deductmediumpackage')
-end)
-
-
-
-RegisterNetEvent('dodobox-getlargepackage')
-AddEventHandler('dodobox-getlargepackage', function(pItemId)
-    TriggerEvent("player:receiveItem", "dodopackagelarge", 1)
-    TriggerServerEvent('dodobox-deductlargepackage')
-end)
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
 
 
